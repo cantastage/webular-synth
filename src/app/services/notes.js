18ee8df9912edd7f504b87c1bae98bd82352c5dd
Gabriele Antonacci <gabrieleantonacci@MacBook-Air-di-massimo.local>
2018-12-14 16:18:02 +0100

    comments

--- src/app/services/notes.js
+++ src/app/services/notes.js
@@ -1,96 +1,96 @@
-var midi, data;
-var frequency;
+// var midi, data;
+// var frequency;
 
-//MIDI IMPLEMENTATION
+// //MIDI IMPLEMENTATION
 
-if (navigator.requestMIDIAccess) {
-    navigator.requestMIDIAccess({
-        sysex: false
-    }).then(onMIDISuccess, onMIDIFailure);
-} else {
-    alert("No MIDI support in your browser.");
-}
+// if (navigator.requestMIDIAccess) {
+//     navigator.requestMIDIAccess({
+//         sysex: false
+//     }).then(onMIDISuccess, onMIDIFailure);
+// } else {
+//     alert("No MIDI support in your browser.");
+// }
 
-function onMIDISuccess(midiAccess) {
-    midi = midiAccess; 
+// function onMIDISuccess(midiAccess) {
+//     midi = midiAccess; 
 
-    var inputs = midi.inputs.values();
-    for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
-        input.value.onmidimessage = onMIDIMessage;
-    }
-}
+//     var inputs = midi.inputs.values();
+//     for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
+//         input.value.onmidimessage = onMIDIMessage;
+//     }
+// }
 
-function onMIDIFailure(error) {
-    console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + error);
-}
+// function onMIDIFailure(error) {
+//     console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + error);
+// }
 
-function onMIDIMessage(event) {
-  data = event.data; 
-  channel = data[0] & 0xf;
-  type = data[0] & 0xf0;
-  note = data[1];
-  velocity = data[2];
-  document.querySelector(".circle").classList.toggle("active");
+// function onMIDIMessage(event) {
+//   data = event.data; 
+//   channel = data[0] & 0xf;
+//   type = data[0] & 0xf0;
+//   note = data[1];
+//   velocity = data[2];
+//   document.querySelector(".circle").classList.toggle("active");
   
-  switch (type) {
-        case 144: // noteOn message 
-             noteOn(note, velocity);
-             break;
-        case 128: // noteOff message 
-            noteOff(note, velocity);
-            break;
-    }
-}
+//   switch (type) {
+//         case 144: // noteOn message 
+//              noteOn(note, velocity);
+//              break;
+//         case 128: // noteOff message 
+//             noteOff(note, velocity);
+//             break;
+//     }
+// }
 
-function noteOn(midiNote, velocity){
-  frequency = 440* Math.pow(2, (midiNote-69)/12);
-  playNote(frequency, velocity);
-  //console.log(frequency);
-}
+// function noteOn(midiNote, velocity){
+//   frequency = 440* Math.pow(2, (midiNote-69)/12);
+//   playNote(frequency, velocity);
+//   //console.log(frequency);
+// }
 
-function noteOff(midiNote, velocity){
-  osc.stop();
-}
+// function noteOff(midiNote, velocity){
+//   osc.stop();
+// }
 
-//AUDIO GENERATOR
-c = new AudioContext();
-function playNote(frequency, velocity){
-  console.log("call function")
-  osc = c.createOscillator();
-  osc.frequency.value = frequency;
-  g = c.createGain();
-  g.gain.value = velocity * 4 / 127;
-  osc.connect(g);
-  g.connect(c.destination);
-  now=c.currentTime;
-  osc.start(now);
-}
+// //AUDIO GENERATOR
+// c = new AudioContext();
+// function playNote(frequency, velocity){
+//   console.log("call function")
+//   osc = c.createOscillator();
+//   osc.frequency.value = frequency;
+//   g = c.createGain();
+//   g.gain.value = velocity * 4 / 127;
+//   osc.connect(g);
+//   g.connect(c.destination);
+//   now=c.currentTime;
+//   osc.start(now);
+// }
 
 
-window.addEventListener("keydown", onKeyDown);
-window.addEventListener("keyup", onKeyUp);
+// window.addEventListener("keydown", onKeyDown);
+// window.addEventListener("keyup", onKeyUp);
 
-var code;
+// var code;
 
-function onKeyDown(e){
-  code = String.fromCharCode(e.keyCode)
-  if (e.repeat)
-    return;
+// function onKeyDown(e){
+//   code = String.fromCharCode(e.keyCode)
+//   if (e.repeat)
+//     return;
   
-  switch(code){
-    case "A": 
-      frequency = 523.25;
-      playNote(frequency, 100);
-      break;
-    case "B": 
-      alert("B");
-      break;
+//   switch(code){
+//     case "A": 
+//       frequency = 523.25;
+//       playNote(frequency, 100);
+//       break;
+//     case "B": 
+//       alert("B");
+//       break;
       
-  }
+//   }
   
   
-}
+// }
 
-function onKeyUp(){
-  noteOff();
-}
+// function onKeyUp(){
+//   noteOff();
+// }
