a8cbb3037e48391aac6645c8e295f470faadde95
Gabriele Antonacci <gabrieleantonacci@Air-di-massimo.station>
2018-12-15 15:54:18 +0100

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SynthModuleContainerComponent } from './synth-module-container/synth-module-container.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MoogLadderFilterComponent } from './synth-modules/moog-ladder-filter/moog-ladder-filter.component';
 import { ClockComponent } from './synth-modules/clock/clock.component';
 import { SequencerComponent } from './synth-modules/sequencer/sequencer.component';
-import { PolyphonicOscComponent } from './synth-modules/polyphonic-osc/polyphonic-osc.component';
 import { AddModuleDirective } from './directives/add-module.directive';
-import { KnobComponent } from './synth-modules/sub-components/knob/knob.component';
 import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     SynthModuleContainerComponent,
-    MoogLadderFilterComponent,
     ClockComponent,
     SequencerComponent,
-    PolyphonicOscComponent,
     AddModuleDirective,
-    KnobComponent,
   ],
-  entryComponents: [MoogLadderFilterComponent],
+  entryComponents: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // AudioContextModule.forRoot('balanced'),
     DragDropModule,
     FormsModule,
     KnobModule,
