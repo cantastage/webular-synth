90be8f1c9c40f1d5c21525a50fd9a69af69a7030
Dario Cantarelli <dario.cantarelli@mail.polimi.it>
2019-05-15 19:11:39 +0200

    Completed chord name formatter pipe.

--- src/app/pipes/chord-name-pipe.pipe.ts
+++ src/app/pipes/chord-name-pipe.pipe.ts
@@ -10,7 +10,57 @@ import { Chord } from '../model/modules/sequencer/prog/Chord';
 export class ChordNamePipePipe implements PipeTransform {
 
   transform(chord: Chord): String {
-    return null;
+    const chord_root = chord.root.pitchClassName;
+    const chord_quality = chord.quality.chordQualityName;
+    let formatted_quality = '';
+    switch (chord_quality) {
+      case ('maj'):
+        formatted_quality = '';
+        break;
+      case ('min'):
+        formatted_quality = 'm';
+        break;
+      case ('dim'):
+        formatted_quality = 'ᵒ';
+        break;
+      case ('aug'):
+        formatted_quality = '⁺';
+        break;
+        case ('maj7'):
+        formatted_quality = 'ᵐᵃʲ⁷';
+        break;
+        case ('min7'):
+        formatted_quality = 'm⁷';
+        break;
+        case ('halfDim7'):
+        formatted_quality = 'ø⁷';
+        break;
+        case ('aug7'):
+        formatted_quality = '⁺⁷';
+        break;
+        case ('min9'):
+        formatted_quality = 'ᵐ⁹';
+        break;
+        case ('dim9'):
+        formatted_quality = 'ᵒ⁹';
+        break;
+        case ('dim7'):
+        formatted_quality = 'ᵒ⁷';
+        break;
+        case ('aug9'):
+        formatted_quality = '⁺⁹';
+        break;
+        case ('maj9'):
+        formatted_quality = 'ᵐᵃʲ⁹';
+        break;
+        case ('dom7'):
+        formatted_quality = '⁷';
+        break;
+        case ('dom13'):
+        formatted_quality = '¹³';
+        break;
+    }
+    return (chord_root + formatted_quality);
   }
 
 }
